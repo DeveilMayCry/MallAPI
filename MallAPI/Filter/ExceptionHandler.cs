e2cbaf781d0351a069dc1fdf11f6a8b12175bcd7
jack <506587246@qq.com>
2020-01-30 15:18:28 +0800

    引入serilog更改日志记录行为

--- MallAPI/Filter/ExceptionHandler.cs
+++ MallAPI/Filter/ExceptionHandler.cs
@@ -1,6 +1,7 @@
 ﻿using MallAPI.DataModel.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,8 +11,16 @@ namespace MallAPI.Filter
 {
     public class ExceptionHandlerAttribute : ExceptionFilterAttribute
     {
+        private ILogger _logger = null;
+
+        public ExceptionHandlerAttribute(ILogger<ExceptionHandlerAttribute> logger)
+        {
+            _logger = logger;
+        }
+
         public override void OnException(ExceptionContext context)
         {
+            _logger.LogError($"{context.Exception.Message}:{context.Exception.StackTrace}");
             var response = new Response(Enum.ResultEnum.Fail, context.Exception.Message);
             context.Result = new JsonResult(response);
         }
--- MallAPI/Program.cs
+++ MallAPI/Program.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace MallAPI
 {
@@ -20,7 +23,19 @@ namespace MallAPI
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>().UseSerilog((context, configuration) => 
+                    {
+                        configuration
+                        .MinimumLevel.Warning()
+                        // 日志调用类命名空间如果以 Microsoft 开头，覆盖日志输出最小级别为 Information
+                        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                        .Enrich.FromLogContext()
+                        // 配置日志输出到控制台
+                        .WriteTo.Console()
+                        // 配置日志输出到文件，文件输出到当前项目的 logs 目录下
+                        // 日记的生成周期为每天
+                        .WriteTo.File(Path.Combine("logs", @"log.txt"), rollingInterval: RollingInterval.Day);
+                    });
                 });
     }
 }
